94de49a2ab827b10633fa66ad00b388c8ed13d1c
Jurica Papak <jpapak95@gmail.com>
2019-06-25 14:52:19 +0200

    Rework guide_control.py
    Add tracked actor publisher (CircleObstacle msg type)
    Change from print to rospy.log for logging
    Add ros parameters to launch file and to the GuideController

--- nodes/guide_control.py
+++ nodes/guide_control.py
@@ -15,37 +15,34 @@ from obstacle_detector.msg import Obstacles, CircleObstacle
 
 class GuideController:
 
-    ACTOR_TOL = 0.2 # tolerance of actor movement in a timestep
-    GUIDE_DISTANCE = 1  # how far the robot will try to stay ahead of the actor
-    SIGMOID_FACTOR = np.log(99)/GUIDE_DISTANCE # factor for shaping the sigmoid
-
     def __init__(self):
-        # transform buffer
-        self.tf_buffer = tf2_ros.Buffer()
-        # command velocity publisher
-        self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=2)
-        # last known actor location
-        self.actor_loc_x = 0
-        self.actor_loc_y = 0
-        # last known actor velocity
-        self.actor_vel_x = 0
-        self.actor_vel_y = 0
+        # initialize the node and set control rate
+        rospy.init_node('guide_controller')
+        self.mainloop_rate = rospy.Rate(1)
+
+        self.GUIDE_DISTANCE = rospy.get_param("~guide_distance", 2.0)  # how far the robot will try to stay ahead of the actor
+        self.ACTOR_TOL = rospy.get_param("~actor_tolerance", 0.2)  # tolerance of actor movement in a timestep
+        self.SIGMOID_FACTOR = np.log(99)/self.GUIDE_DISTANCE # factor for shaping the sigmoid
+
+        self.tf_buffer = tf2_ros.Buffer()   # transform buffer
+        self.map_frame = rospy.get_param("~map_frame", "map")  # transform frame of the map (static)
+        self.robot_frame = rospy.get_param("~robot_frame", "base_link")  # transform frame of the robot (dynamic)
+        self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=2)  # command velocity publisher
+        self.actor_pub = rospy.Publisher('tracked_actor', CircleObstacle, queue_size=2) # actor location publisher
+        # last known actor location and velocity
+        self.actor = CircleObstacle()
         # last known robot location and heading
         self.robot_loc_x = 0
         self.robot_loc_y = 0
-        self.robot_angle = 0 # [-pi, pi]
+        self.robot_yaw = 0 # [-pi, pi]
         # status of the guide controller:
         # 0 - no actor available for guiding, finding actor
         # 1 - locked on actor, guiding
         self.guide_status = 0
 
-        # initialize the node
-        rospy.init_node('guide_control')
-
         # setup transform buffer and wait for it to fill up
-        rate = rospy.Rate(1)
         listener = tf2_ros.TransformListener(self.tf_buffer)
-        rate.sleep()
+        self.mainloop_rate.sleep()
 
         # determine initial robot pose
         self.get_robot()
@@ -54,10 +51,10 @@ class GuideController:
         rospy.Subscriber('cmd_vel_dwb', Twist, self.cmd_vel_callback, queue_size=1)
         rospy.Subscriber('odom', Odometry, self.odom_callback, queue_size=1)
         rospy.Subscriber('obstacles', Obstacles, self.obstacle_callback, queue_size=1)
-        print ("[GUIDE_CONTROL]: Initialization successful.")
+        rospy.loginfo("GuideController: Initialization successful.")
 
-    # sigmoid defined around keeping the actor at the GUIDE_DISTANCE
     def sigmoid(self, x):
+        '''Sigmoid defined around keeping the actor at the GUIDE_DISTANCE'''
         return 2 / (1 + np.exp(self.SIGMOID_FACTOR*(x-self.GUIDE_DISTANCE)))
 
     def wrap_to_pi(self, angle):
@@ -75,14 +72,14 @@ class GuideController:
 
     # for augmenting cmd_vel according to the actor-robot relation
     def cmd_vel_callback(self, cmd_vel):
-        dist_vector = np.array([self.robot_loc_x - self.actor_loc_x, self.robot_loc_y - self.actor_loc_y])
+        dist_vector = np.array([self.robot_loc_x - self.actor.center.x, self.robot_loc_y - self.actor.center.y])
         # distance correction
         distance = np.linalg.norm(dist_vector)
         self.correct_for_distance(cmd_vel, distance)
         # angle correction
         angle = np.arctan2(dist_vector[1], dist_vector[0])
-        wrapped_angle = self.wrap_to_pi(np.abs(robot_angle - angle))
-        self.correct_for_angle(cmd_vel, wrapped_angle, robot_angle)
+        wrapped_angle = self.wrap_to_pi(np.abs(robot_yaw - angle))
+        self.correct_for_angle(cmd_vel, wrapped_angle, robot_yaw)
 
         self.cmd_vel_pub.publish(cmd_vel)
 
@@ -91,10 +88,10 @@ class GuideController:
         tracked = False
         for circle in obstacles.circles:
             x, y = circle.center.x, circle.center.y
-            if np.abs(x - self.actor_loc_x) < self.ACTOR_TOL and \
-               np.abs(y - self.actor_loc_y) < self.ACTOR_TOL:
-                self.actor_loc_x, self.actor_loc_y = x, y
-                self.actor_vel_x, self.actor_vel_y = circle.velocity.x, circle.velocity.y
+            if np.abs(x - self.actor.center.x) < self.ACTOR_TOL and \
+               np.abs(y - self.actor.center.y) < self.ACTOR_TOL:
+                self.actor.center.x, self.actor.center.y = x, y
+                self.actor.velocity.x, self.actor.velocity.y = circle.velocity.x, circle.velocity.y
                 tracked = True
         if tracked:
             self.guide_status = 1
@@ -103,7 +100,7 @@ class GuideController:
 
     # for updating the robot position
     def odom_callback(self, odom):
-        trans = self.tf_buffer.lookup_transform('map', 'base_link', rospy.Time())
+        trans = self.tf_buffer.lookup_transform(self.map_frame, self.robot_frame, rospy.Time())
 
         self.robot_loc_x = trans.transform.translation.x
         self.robot_loc_y = trans.transform.translation.y
@@ -111,7 +108,7 @@ class GuideController:
         quaternion = (trans.transform.rotation.x, trans.transform.rotation.y,
                       trans.transform.rotation.z, trans.transform.rotation.w)
         euler = tf.transformations.euler_from_quaternion(quaternion)
-        self.robot_angle = euler[2] # yaw angle used for robot heading
+        self.robot_yaw = euler[2] # yaw angle used for robot heading
 
     def get_robot(self):
         try:
@@ -119,7 +116,8 @@ class GuideController:
             # use the odom_callback function for updating the globals
             self.odom_callback(odom)
         except rospy.ROSException:
-            print ("[GUIDE_CONTROL]: Robot pose couldn't be retrieved in time.")
+            rospy.logerr("GuideController: Robot pose couldn't be retrieved in time. Check /tf topic.")
+            raise RuntimeError("GuideController failed initialization.")
 
     # actor location inialization; uses closest actor behind the robot
     def get_actor(self):
@@ -130,7 +128,7 @@ class GuideController:
             for circle in obstacles.circles:
                 dist_vector = np.array([self.robot_loc_x - circle.center.x, self.robot_loc_y - circle.center.y])
                 angle = np.arctan2(dist_vector[1], dist_vector[0])
-                wrapped_angle = self.wrap_to_pi(np.abs(self.robot_angle - angle))
+                wrapped_angle = self.wrap_to_pi(np.abs(self.robot_yaw - angle))
                 if np.abs(wrapped_angle) < 1:
                     distance = np.linalg.norm(dist_vector)
                     if distance < min_distance:
@@ -138,29 +136,29 @@ class GuideController:
                         min_distance = distance
 
             if best_candidate == None:
-                print ("[GUIDE_CONTROL]: No suitable actor found! Trying again...")
-                return False
+                rospy.loginfo("GuideController: No suitable actor found! Trying again...")
             else:
-                self.actor_loc_x = best_candidate[0]
-                self.actor_loc_y = best_candidate[1]
+                self.actor.center.x = best_candidate[0]
+                self.actor.center.y = best_candidate[1]
                 self.guide_status = 1 # tracking
-                print ("[GUIDE_CONTROL]: Actor at location (%3f,%3f) chosen." % (self.actor_loc_x, self.actor_loc_y))
-                return True
+                rospy.loginfo("GuideController: Actor at location (%1.2f, %1.2f) chosen." % (self.actor.center.x, self.actor.center.y))
 
         except rospy.ROSException:
-            print ("[GUIDE_CONTROL]: Message couldn't be recieved in time. Check /obstacles topic.")
-            return False
+            rospy.loginfo("GuideController: Message couldn't be recieved in time. Check /obstacles topic.")
+
+    def run(self):
+        while(1):
+            if self.guide_status == 1:
+                rospy.loginfo("GuideController: Tracking actor at location (%1.2f, %1.2f)." % (self.actor.center.x, self.actor.center.y))
+                self.actor_pub.publish(self.actor) # publish actor info
+            elif self.guide_status == 0:
+                rospy.loginfo("GuideController: No actor is being tracked, searching for new actor behind robot.")
+                self.get_actor()
+            self.mainloop_rate.sleep()
 
 if __name__ == '__main__':
     try:
         controller = GuideController()
-        mainloop_rate = rospy.Rate(1)
-        while(1):
-            if controller.guide_status == 1:
-                print("[GUIDE_CONTROL]: Actor tracked at (%f %f)" % (controller.actor_loc_x, controller.actor_loc_y))
-            elif controller.guide_status == 0:
-                controller.get_actor()
-            mainloop_rate.sleep()
-
+        controller.run()
     except rospy.ROSInterruptException:
         pass
